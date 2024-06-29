44fbcff8a2d9e2636999ab987a79519a4ef58aca
valentin-morette <morette.valentin@gmail.com>
2024-06-29 15:56:13 +0200

    sell ùmax pokemon

--- backend/src/controllers/PokemonController.js
+++ backend/src/controllers/PokemonController.js
@@ -438,43 +438,112 @@ class PokemonController {
       });
   };
 
-  static sellPokemon = (req, res) => {
-    const { namePokemon } = req.body;
-    const { idTrainer } = req.body;
-    const { quantity } = req.body;
-    const { isShiny } = req.body;
-    let idPokemon = 0;
-    models.pokemon.findByName(namePokemon).then(([resultId]) => {
+  // static sellPokemon = (req, res) => {
+  //   const { namePokemon } = req.body;
+  //   const { idTrainer } = req.body;
+  //   let { quantity } = req.body;
+  //   const { isShiny } = req.body;
+  //   const { max } = req.body;
+  //   let idPokemon = 0;
+  //   models.pokemon.findByName(namePokemon).then(([resultId]) => {
+  //     if (resultId.length === 0) {
+  //       res.status(201).send({ status: "noExistPokemon" });
+  //       return;
+  //     }
+  //     idPokemon = resultId[0].id;
+  //     models.pokemon_trainer
+  //       .updateDownQuantity(idPokemon, idTrainer, quantity, isShiny)
+  //       .then(([result]) => {
+  //         if (result.affectedRows === 0) {
+  //           res.status(201).send({ status: "noPokemon" });
+  //         } else {
+  //           models.pokemon.find(idPokemon).then(([resultPokemon]) => {
+  //             const sellPrice =
+  //               resultPokemon[0].sellPrice * quantity * (isShiny ? 3 : 1);
+  //             models.trainer.updateMoney(idTrainer, sellPrice).then(() => {
+  //               res.status(201).send({
+  //                 status: "sell",
+  //                 pokemonName: resultPokemon[0].name,
+  //                 sellPrice,
+  //               });
+  //             });
+  //           });
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.error(err);
+  //         res.sendStatus(500);
+  //       });
+  //   });
+  // };
+
+  static sellPokemon = async (req, res) => {
+    const { namePokemon, idTrainer, isShiny, max } = req.body;
+    let { quantity } = req.body;
+
+    try {
+      const [resultId] = await models.pokemon.findByName(namePokemon);
       if (resultId.length === 0) {
         res.status(201).send({ status: "noExistPokemon" });
         return;
       }
-      idPokemon = resultId[0].id;
-      models.pokemon_trainer
-        .updateDownQuantity(idPokemon, idTrainer, quantity, isShiny)
-        .then(([result]) => {
-          if (result.affectedRows === 0) {
-            res.status(201).send({ status: "noPokemon" });
-          } else {
-            models.pokemon.find(idPokemon).then(([resultPokemon]) => {
-              const sellPrice =
-                resultPokemon[0].sellPrice * quantity * (isShiny ? 3 : 1);
-              models.trainer.updateMoney(idTrainer, sellPrice).then(() => {
-                res.status(201).send({
-                  status: "sell",
-                  pokemonName: resultPokemon[0].name,
-                  sellPrice,
-                });
-              });
-            });
-          }
-        })
-        .catch((err) => {
-          console.error(err);
-          res.sendStatus(500);
-        });
-    });
+      const idPokemon = resultId[0].id;
+
+      // Si `max` est spécifié, vous devriez probablement ajuster la `quantity` ici
+      if (max) {
+        const [maxQtyResult] = await models.pokemon_trainer.findQuantity(
+          idPokemon,
+          idTrainer,
+          isShiny
+        );
+        quantity = maxQtyResult[0] ? maxQtyResult[0].quantity : 0;
+      }
+
+      const [updateResult] = await models.pokemon_trainer.updateDownQuantity(
+        idPokemon,
+        idTrainer,
+        quantity,
+        isShiny
+      );
+      if (updateResult.affectedRows === 0) {
+        res.status(201).send({ status: "noPokemon", quantity });
+        return;
+      }
+
+      const [resultPokemon] = await models.pokemon.find(idPokemon);
+      const sellPrice =
+        resultPokemon[0].sellPrice * quantity * (isShiny ? 3 : 1);
+
+      await models.trainer.updateMoney(idTrainer, sellPrice);
+
+      res.status(201).send({
+        status: "sell",
+        pokemonName: resultPokemon[0].name,
+        sellPrice,
+        quantity,
+      });
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
   };
 }
+
+// if (max) {
+//   const [result] = await models.pokemon_trainer.findQuantity(
+//     pokemon[0].id,
+//     idTrainer,
+//     isShiny
+//   );
+//   if (result.length === 0) {
+//     res.status(201).send({
+//       status: "noPokemon",
+//       numberPokemon: pokemon[0].numberEvolution,
+//       quantity: 1,
+//     });
+//     return;
+//   }
+//   quantity = Math.floor(result[0].quantity / pokemon[0].numberEvolution);
+// }
 
 module.exports = PokemonController;
